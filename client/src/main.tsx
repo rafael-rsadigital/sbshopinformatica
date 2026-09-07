597a1450211a12e80ac1f6884b36c1040e509a79
RSA Digital <atendimento.rsadigital@gmail.com>
2026-09-07 21:55:49 +0000

    SEO: hidratar HTML pré-renderizado no cliente

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./local-assets.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+hydrateRoot(document.getElementById("root")!, <App />);
